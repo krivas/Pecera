dd051c6a31381c7d562028c111f4c9e862baddc9
Kevin Rivas <krivas.reyes@gmail.com>
2015-10-13 20:29:51 -0400

    Refactor al movimiento de los peces

--- Pecera/Modelo/AnimalAcuatico.cs
+++ Pecera/Modelo/AnimalAcuatico.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Pecera.Modelo
 {
@@ -11,103 +12,327 @@ namespace Pecera.Modelo
     {
 
         protected PictureBox aquaticAnimal=new PictureBox();
+        private bool upperBound;
+        private bool lowBound;
+        private bool leftBound;
+        private bool rightBound;
+        private enum Route{ DiagonalUp=1,DiagonalUpDown=2,DiagonalDown=3,DiagonalDownUp=4   };
+        private Route route;
         public static int quantity;
-         private void moverDiagonalArriba(Form form)
+       
+       
+       
+         private void moveDiagonalUp(Form form)
         {
+            Route temp = Route.DiagonalUp;
             aquaticAnimal.Parent = form;
             aquaticAnimal.Left += 1;
             aquaticAnimal.Top -= 1;
-        }
+            checkingDiagonalLimits(form,temp);
 
-         private void moverDiagonalAbajo(Form form)
+  
+        }
+         private void moveDiagonalUpDown(Form form)
+         {
+             Route temp = Route.DiagonalUp;
+             aquaticAnimal.Parent = form;
+             aquaticAnimal.Left -= 1;
+             aquaticAnimal.Top += 1;
+             checkingDiagonalLimits(form,temp);
+         }
+
+         private void moveDiagonalDown(Form form)
+         {
+             Route temp = Route.DiagonalDown;
+             aquaticAnimal.Parent = form;
+             aquaticAnimal.Left += 1;
+             aquaticAnimal.Top += 1;
+             checkingDiagonalLimits(form,temp);
+         }
+
+        
+
+        private void moveDiagonalDownUp(Form form)
         {
-            aquaticAnimal.Parent = form;
-            aquaticAnimal.Left += 1;
-            aquaticAnimal.Top += 1;
+            Route temp = Route.DiagonalDownUp;
+           aquaticAnimal.Parent = form;
+             aquaticAnimal.Left -= 1;
+             aquaticAnimal.Top -= 1;
+             checkingDiagonalLimits(form,temp);
+            
         }
 
-         private void moverDerecho(Form form)
+       
+
+         private void moveRight(Form form)
         {
               aquaticAnimal.Parent = form;
              aquaticAnimal.Left += 1;
+
+             if (aquaticAnimal.Left >= form.Width-90)
+             {
+                 leftBound = false;
+                 rightBound = true;
+                 Image flipImage = aquaticAnimal.Image;
+                 flipImage.RotateFlip(RotateFlipType.Rotate180FlipY);
+                 aquaticAnimal.Image = flipImage;
+
+             }
         }
 
-         private void moverIzquierda(Form form)
+         private void moveLeft(Form form)
         {
             aquaticAnimal.Parent = form;
             aquaticAnimal.Left -= 1;
+
+            if (aquaticAnimal.Left <=20)
+            {
+                leftBound = true;
+                rightBound = false;
+                Image flipImage = aquaticAnimal.Image;
+                 flipImage.RotateFlip(RotateFlipType.Rotate180FlipY);
+                 aquaticAnimal.Image = flipImage;
+
+            }
         }
 
-         private void moverArriba(Form form)
+         private void moveUp(Form form)
         {
             aquaticAnimal.Parent = form;
             aquaticAnimal.Top -= 1;
+
+            if (aquaticAnimal.Top <= 200)
+            {
+                lowBound = false;
+                upperBound = true;
+
+            }
+
         }
 
-         private void moverAbajo(Form form)
+         private void moveDown(Form form)
         {
             aquaticAnimal.Parent = form;
             aquaticAnimal.Top += 1;
+
+
+            if (aquaticAnimal.Top >= form.Height - 50)
+            {
+
+                lowBound = true;
+                upperBound = false;
+
+            }
+           
         }
 
-         public void mover(Form form)
+         public void move(Form form)
          {
              if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\Dorys.png"))
              {
-                 moverDiagonalArriba(form);
-                 return;
+                 proofDiagonalLimits(form);
+               
              }
              else if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\Pez hembra.png"))
              {
-                 moverIzquierda(form);
-                 return;
+
+                 proofDiagonalLimits(form);
              }
-             else if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\Tiburon adulto.png"))
+             else if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\tiburon adulto.png"))
              {
-                 moverDerecho(form);
-                 return;
+                 proofLimits(form);
+               
              }
-             else if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\Tiburon macho.png"))
-             {
-                 moverDerecho(form);
-                 return;
-             }
+            
              else if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\Tiburon bebe hembra.png"))
              {
-                 moverIzquierda(form);
-                 return;
+                 proofLimits(form);
+               
              }
              else if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\Nemo.png"))
              {
-                 moverArriba(form);
-                 return;
+
+                 proofLimits(form);
+
+               
              }
              else if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\tiburon macho.png"))
              {
-                 moverAbajo(form);
-                 return;
+                 proofLimits(form);
+
+                 
              }
 
-             else if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\tiburon adulto.png"))
-             {
-                 moverDerecho(form);
-                 return;
-             }
+             
              else if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\Pez macho.png"))
              {
-                 moverDiagonalAbajo(form);
-                 return;
+                 proofLimits(form);
+
+                
              }
 
              else if (aquaticAnimal.ImageLocation.Equals(@"C:\Users\Kevin\Desktop\Construccion\Pecera\Pecera\Resources\tiburon hembra.png"))
              {
-                 moverIzquierda(form);
+                 proofLimits(form);
                  return;
              }
          }
 
-       
 
         
+
+         private void proofLimits(Form form)
+         {
+             if (upperBound == true)
+                 moveDown(form);
+
+             else if(lowBound == true)
+                 moveUp(form);
+
+              else if (leftBound == true)
+                 moveRight(form);
+
+             else if (rightBound == true)
+                 moveLeft(form);
+             
+         }
+
+         private void proofDiagonalLimits(Form form)
+         {
+
+             if (upperBound == true && route == Route.DiagonalUp)
+                 moveDiagonalDown(form);
+             else if (upperBound == true && route == Route.DiagonalDownUp)
+                 moveDiagonalUpDown(form);
+
+             else if (lowBound == true && route == Route.DiagonalDown)
+                 moveDiagonalUp(form);
+
+             else if (lowBound == true && route == Route.DiagonalUpDown)
+                 moveDiagonalDownUp(form);
+
+             else if (leftBound == true && route == Route.DiagonalUpDown)
+                 moveDiagonalDown(form);
+
+             else if (leftBound == true && route == Route.DiagonalDownUp)
+                 moveDiagonalUp(form);
+
+             else if (rightBound == true && route == Route.DiagonalDown)
+                 moveDiagonalUpDown(form);
+
+             else if (rightBound == true && route == Route.DiagonalUp)
+                 moveDiagonalDownUp(form);
+
+
+           
+         }
+
+         private void checkingDiagonalLimits(Form form,Route temp)
+         {
+             if (aquaticAnimal.Top <= 200)
+             {
+                 lowBound = false;
+                 upperBound = true;
+                 
+                 route = temp;
+
+             }
+
+              if (aquaticAnimal.Top >= form.Height - 50 )
+             {
+
+                 lowBound = true;
+                 upperBound = false;
+                
+                 route = temp;
+
+             }
+
+              if (aquaticAnimal.Left >= form.Width - 90 )
+             {
+                 
+                 leftBound = false;
+                 rightBound = true;
+               
+                 route = temp;
+
+
+             }
+
+              if (aquaticAnimal.Left <= 20)
+             {
+                 leftBound = true;
+                 rightBound = false;
+               
+                 route = temp;
+                  
+             }
+
+             
+         }
+
+         public void initializeDirection(string direction)
+         {
+            
+             if (direction.Equals("up"))
+             {
+                 lowBound = true;
+                 upperBound = false;
+              
+                 
+             }
+             else if (direction.Equals("down"))
+             {
+                 lowBound = false;
+                 upperBound = true;
+             
+             }
+
+             else if (direction.Equals("left"))
+             {
+                 leftBound = false;
+                 rightBound = true;
+
+             }
+
+             else if (direction.Equals("right"))
+             {
+                 leftBound = true;
+                 rightBound = false;
+
+             }
+
+              else if (direction.Equals("DiagonalUp"))
+             {
+                 lowBound = true;
+                 upperBound = false;
+                  rightBound=false;
+                  leftBound=false;
+                 route = Route.DiagonalDown;
+               
+
+             }
+
+             else if (direction.Equals("DiagonalDown"))
+             {
+                 lowBound = false;
+                 upperBound = true;
+                 rightBound = false;
+                 leftBound = false;
+                 route = Route.DiagonalUp;
+
+             }
+             else if (direction.Equals("DiagonalUpDown"))
+             {
+                 rightBound = true;
+                 leftBound = false;
+                 lowBound = false;
+                 upperBound = false;
+                 route = Route.DiagonalDown;
+
+             }
+         }
+        
     }
+  
 }
